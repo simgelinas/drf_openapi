c01d911f69a97e95161a28fab48f9f6cea4c1b21
colinmcorcoran <colin@openfolio.com>
2018-03-19 17:52:23 -0400

    roll back view

--- drf_openapi/views.py
+++ drf_openapi/views.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from rest_framework import response, permissions
-from rest_framework.schemas import SchemaGenerator
 from rest_framework.renderers import CoreJSONRenderer
 from rest_framework.views import APIView
 
@@ -9,25 +8,15 @@ from drf_openapi.entities import OpenApiSchemaGenerator
 
 
 class SchemaView(APIView):
-
-    def __init__(self,
-                 url='',
-                 title='API Documentation',
-                 renderer_classes=(CoreJSONRenderer, SwaggerUIRenderer, OpenAPIRenderer),
-                 permission_classes=(permissions.IsAdminUser,),
-                 generator_class=OpenApiSchemaGenerator,
-                 **generator_class_kwargs):
-        if not issubclass(generator_class, SchemaGenerator):
-            raise Exception('Generator class must extend rest_framework.schemas.SchemaGenerator')
-        self.generator_class=generator_class
-        self.generator_class_kwargs = generator_class_kwargs
-        super(SchemaView, self).__init__(url=url, title=title, renderer_classes=renderer_classes, permission_classes=permission_classes)
+    renderer_classes = (CoreJSONRenderer, SwaggerUIRenderer, OpenAPIRenderer)
+    permission_classes = (permissions.IsAdminUser,)
+    url = ''
+    title = 'API Documentation'
 
     def get(self, request, version):
-        generator = self.generator_class(
+        generator = OpenApiSchemaGenerator(
             version=version,
             url=self.url,
-            title=self.title,
-            **self.generator_class_kwargs
+            title=self.title
         )
         return response.Response(generator.get_schema(request))
